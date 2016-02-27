0affeed6edc74f47e762332212fd0e49cf931c8c
masaibar <orebatemoko@gmail.com>
2016-02-27 19:06:33 +0900

    JSONダウンロードするところまで

--- app/src/main/java/com/masaibar/jsondownloadsample/MainActivity.java
+++ app/src/main/java/com/masaibar/jsondownloadsample/MainActivity.java
@@ -1,12 +1,28 @@
 package com.masaibar.jsondownloadsample;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.client.HttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL_JSON = "https://raw.githubusercontent.com/masaibar/SampleJSON/master/sample.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +33,70 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("!!!", "button pushed");
+                new GetJSONAsyncTask(URL_JSON, MainActivity.this).execute();
             }
         });
+    }
+
+    private class GetJSONAsyncTask extends AsyncTask<Void, Void, String> {
+
+        private String mUrl;
+        private Activity mActivity; //画面更新不要ならいらないかも
+
+        public GetJSONAsyncTask(String url, Activity activity) {
+            mUrl = url;
+            mActivity = activity;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpsURLConnection connection;
+            String result = "";
+            try {
+                URL url = new URL(mUrl);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+
+                int response = connection.getResponseCode();
+
+                result = readStream(connection.getInputStream());
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d("!!!", "result = " + result);
+
+            TextView textView = (TextView) mActivity.findViewById(R.id.text_result);
+            textView.setText(result);
+        }
+
+        private String readStream(InputStream inputStream) {
+            if (inputStream == null) {
+                return null;
+            }
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+
+            try {
+                while((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return stringBuffer.toString();
+        }
     }
 }
